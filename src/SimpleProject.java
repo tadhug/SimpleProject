c10463870dc1727867f8ab577013007f5431874a
Thaddaeus <tadhug@gmail.com>
2017-04-07 16:35:07 -0700

    add files

---
+++ src/SimpleProject.java
@@ -0,0 +1,24 @@
+//This is a project by Thaddaeus Hug
+
+public class SimpleProject {
+
+	public static void main(String[] args) 
+	{
+		int apples = 64 / 8;
+		int oranges = 5 + 3;
+		if (oranges > apples) {System.out.println("There are more oranges than apples");}
+		else if (oranges == apples) {System.out.println("There are an equal number of apples and oranges.");}
+		else {System.out.println("There are more apples than oranges");}
+		
+		{
+			System.out.println("This is the total number of pieces of fruit there are:");
+			System.out.println(oranges + apples);
+}
+
+/*
+This is a long comment using the star-slash method of comments
+ */
+
+	}
+
+}
